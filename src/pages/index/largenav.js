e98d9feed7d92895ea6451be5865556d95fb4cd3
Michael Bowman <bowmanmc@gmail.com>
2019-10-13 14:47:06 -0400

    use <Link /> instead of <a /> for internal links

--- src/pages/index/largenav.js
+++ src/pages/index/largenav.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import './largenav.scss';
 
 export default () => {
     return (
         <div className="largenav">
-            <a href="/resume">
+            <Link to="/resume">
                 <h2>Resume</h2>
                 <p>Check out my professional skills &amp; work history.</p>
-            </a>
+            </Link>
 
-            <a href="/projects">
+            <Link to="/projects">
                 <h2>Projects</h2>
                 <p>See what I've been spending my free time on lately.</p>
-            </a>
+            </Link>
 
-            <a href="/writing">
+            <Link to="/writing">
                 <h2>Writing</h2>
                 <p>Brainwave transmissions from me to you.</p>
-            </a>
+            </Link>
         </div>
     );
 };
