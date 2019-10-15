18224d90a50e1dd87990fef1e63f696676b9ab09
Michael Bowman <bowmanmc@gmail.com>
2019-10-14 20:53:03 -0400

    projects placeholder

---
+++ src/pages/projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Footer from '../footer';
+import Navbar from '../navbar';
+
+export default () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Projects</h1>
+            <p>
+                Check back later...
+            </p>
+            <Footer />
+        </>
+    );
+};
