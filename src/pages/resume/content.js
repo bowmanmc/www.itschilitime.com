755d0647bdb5614feaa6026166f131d2718be88a
Michael Bowman <bowmanmc@gmail.com>
2019-11-17 11:12:45 -0500

    resume experience

--- src/pages/resume/content.js
+++ src/pages/resume/content.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Experience from './experience';
+
 import './content.scss';
 
 export default () => {
@@ -81,82 +83,166 @@ export default () => {
 
             <section>
                 <h2>Experience</h2>
-                September 2017 – present
-                Mile Two
-                Director of Engineering
-                Helped grow the engineering team at Mile Two from seven to
-                twenty-one developers. Created and ran the Mile Two Internship
-                Program, bringing in entry-level developers and leveling them up
-                to high-performing software engineers. Served as Technical Team
-                Lead for several successful projects for both Commercial and
-                Department of Defense customers.
-                January 2014 – present
-                Sinclair Community College
-                Adjunct Faculty
-                Adjunct professor for the Computer Information Systems
-                department, teaching an evening programming course one night a
-                week. Instructed students from a wide range of backgrounds,
-                ages, and skill levels. Courses taught include Introduction to
-                Problem Solving and Computer Programming (C++), Web Development
-                with HTML and CSS, C++ Software Development, and Java Software
-                Development. Volunteered to assist extracurricular hackathon
-                teams and served on the Software Engineering Advisory Board,
-                helping set the direction for curriculum and program updates.
-                January 2014 – September 2017
-                Applied Information Sciences
-                Principal Software Engineer
-                Security Clearance: TS/SCI. Principal Software Engineer for
-                several contracts for the National Geospatial-Intelligence
-                Agency (NGA) and the National Air and Space Intelligence Center
-                (NASIC) developing web-based mapping and data visualization
-                applications for intelligence analysts. Worked closely with end
-                users to understand their job functions and needs first, then
-                built tools to help distill a large amount of complex data into
-                a useful format and answer key intelligence questions.
-                February 2011 – January 2014
-                Booz Allen Hamilton
-                Principal Software Engineer
-                Security Clearance: TS/SCI. Lead software developer for NGA
-                Innovision’s GIAT Research and Development lab at NASIC. Led an
-                agile, 10 person team responsible for the rapid prototyping of
-                software to provide new capabilities for use across the
-                intelligence community. Performed a wide variety of software
-                development tasks, from gathering, mining, and warehousing
-                extremely large datasets to designing and developing
-                forward-looking user interfaces using front-end web
-                technologies.
-                September 2010 – February 2011
-                Moreover Technologies
-                Senior Software Engineer
-                Developed software in Java for a news and social media startup.
-                Wrote specialized “Harvesters” for scraping specific types of
-                content off of websites (blogs, news articles, and used car
-                listings). Used cloud computing technologies (Amazon EC2) to
-                scale out content harvesting and data mining tasks. Worked as a
-                part of a distributed development team with members in Dayton,
-                Washington D.C., and London.
-                March 2007 – September 2010
-                General Dynamics
-                Principal Software Engineer
-                Security Clearance: TS/SCI. Worked as a technical lead for the
-                user interface team on a number of projects supporting NASIC. In
-                addition to my programming duties, I mentored junior members of
-                the team on software engineering best practices and drove the
-                adoption of agile software development techniques such as
-                Test-Driven Development, Continuous Integration, and Scrum,
-                leading to higher quality software developed faster.
+                <Experience
+                    company="Mile Two"
+                    title="Director of Engineering"
+                    start="September 2017"
+                    end="Present"
+                    description={`
+Helped grow the company from a team of eight into an organization
+of over fifty people. 
+Set technical direction for the engineering staff and helped foster
+a culture of continuous learning and improvement.
+Created and ran the Mile Two Internship
+Program, bringing in entry-level developers and leveling them up
+to high-performing software engineers. 
+Served as Technical Team
+Lead for several successful projects for both Commercial and
+Department of Defense customers.
+                    `}
+                />
+
+                <Experience
+                    company="Sinclair Community College"
+                    title="Adjunct Faculty"
+                    start="January 2014"
+                    end="Present"
+                    description={`
+Adjunct professor for the Computer Information Systems
+department, teaching an evening programming course one night a
+week. Instructed students from a wide range of backgrounds,
+ages, and skill levels. Courses taught include Introduction to
+Problem Solving and Computer Programming (C++), Web Development
+with HTML and CSS, C++ Software Development, and Java Software
+Development. Volunteered to assist extracurricular hackathon
+teams and served on the Software Engineering Advisory Board,
+helping set the direction for curriculum and program updates.
+                    `}
+                />
+
+                <Experience
+                    company="Applied Information Sciences"
+                    title="Principal Software Engineer"
+                    start="January 2014"
+                    end="September 2017"
+                    description={`
+Technical Team Lead and 
+Principal Software Engineer for
+several contracts for the National Geospatial-Intelligence
+Agency (NGA) and the National Air and Space Intelligence Center
+(NASIC) developing web-based mapping and data visualization
+applications for intelligence analysts. Worked closely with end
+users to understand their job functions and needs first, then
+built tools to help distill a large amount of complex data into
+a useful format and answer key intelligence questions.
+                    `}
+                />
+
+                <Experience
+                    company="Booz Allen Hamilton"
+                    title="Principal Software Engineer"
+                    start="February 2011"
+                    end="January 2014"
+                    description={`
+Lead software developer for NGA
+Innovision’s GIAT Research and Development lab at NASIC. Led an
+agile, ten-person team responsible for the rapid prototyping of
+software to provide new capabilities for use across the
+intelligence community. Performed a wide variety of software
+development tasks, from gathering, mining, and warehousing
+extremely large datasets to designing and developing
+forward-looking user interfaces using front-end web
+technologies.
+                    `}
+                />
+
+                <Experience
+                    company="Moreover Technologies"
+                    title="Senior Software Engineer"
+                    start="September 2010"
+                    end="February 2011"
+                    description={`
+Developed software in Java for a news and social media startup.
+Wrote specialized “Harvesters” for scraping specific types of
+content off of websites (blogs, news articles, and used car
+listings). Used cloud computing technologies (Amazon EC2) to
+scale out content harvesting and data mining tasks. Worked as a
+part of a distributed development team with members in Dayton,
+Washington D.C., and London.
+                    `}
+                />
+
+                <Experience
+                    company="General Dynamics"
+                    title="Principal Software Engineer"
+                    start="March 2007"
+                    end="September 2010"
+                    description={`
+Worked as a technical lead for the
+user interface team on a number of projects supporting NASIC. In
+addition to my programming duties, I mentored junior members of
+the team on software engineering best practices and drove the
+adoption of agile software development techniques such as
+Test-Driven Development, Continuous Integration, and Scrum,
+leading to higher quality software developed faster.
+                    `}
+                />
+
+                <Experience
+                    company="Engyro"
+                    title="Senior Software Engineer"
+                    start="March 2006"
+                    end="March 2007"
+                    description={`
+Developed several of the Engyro non-Windows monitoring agents 
+for Microsoft's System Center Operations Manager, allowing 
+customers to monitor their Linux/Unix servers and software 
+(such as Apache HTTPD and Oracle DBMS) alongside their Microsoft 
+assets. Worked in a very dynamic environment where schedules and 
+features were constantly changing based upon customer feedback 
+and needs.  Engyro was acquired by Microsoft in March 2007.
+                    `}
+                />
+
+                <Experience
+                    company="Red Hat"
+                    title="Software Engineer"
+                    start="August 2004"
+                    end="March 2006"
+                    description={`
+Worked on the Red Hat Network Satellite Server product helping to 
+deliver bug fixes and new features to customers on a tight release 
+schedule. I worked on the server-side team, responsible for developing 
+the web interface and APIs to allow system administrators to update 
+and manage a large network of Red Hat Linux machines easily. Day to 
+day activities consisted of writing code for the Java based web 
+application, documenting code and best practices, writing test plans, 
+preparing level of effort time estimates for project management, and 
+helping team members debug code.
+                    `}
+                />
+
+                <Experience
+                    company="Belcan"
+                    title="Web Developer"
+                    start="June 2001"
+                    end="August 2004"
+                    description={`
+Developed and maintained corporate intranet site.
+                    `}
+                />
             </section>
 
             <section>
                 <h2>Education and Certifications</h2>
-                <p>
-                    May 2013
-                    Wright State University
-                    Master of Science, Computer Science
-                    May 2004
-                    Miami University
-                    Bachelor of Science, Computer Science
-                </p>
+                <div className="resume__education">
+                    <h3>Master of Science, Computer Science</h3>
+                    <h4>Wright State University - May 2013</h4>
+                </div>
+                <div className="resume__education">
+                    <h3>Bachelor of Science, Computer Science</h3>
+                    <h4>Miami University - May 2004</h4>
+                </div>
             </section>
         </div>
     );
