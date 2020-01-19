9c0c48eb3a297005188cc4cde6ebaefe4bdd618e
Michael Bowman <bowmanmc@gmail.com>
2020-01-19 14:49:43 -0500

    work on the projects page

--- src/pages/projects/descriptions.js
+++ src/pages/projects/descriptions.js
@@ -16,17 +16,38 @@ export default [{
     name: "Data Visualization Workflows",
     date: "Winter 2019",
     url: "https://github.com/bowmanmc/dvworkflows",
+    preview: 'preview-dvworkflows.png',
+    links: [{
+        url: 'https://github.com/bowmanmc/dvworkflows',
+        title: 'GitHub Repository'
+    }, {
+        url: 'https://www.meetup.com/Gem-City-JS',
+        title: 'Gem City JS'
+    }],
     description: `
+        This is a repository containing slides and example code for a talk I gave at
+        Gem City JS in January of 2019 on common Data Visualization workflows for
+        the web. I gave examples using React, Angular, and Vue, along with other
+        common Data Visualization tools such as Tableau and Adobe Illustrator / Affinity Designer.
+        It may be confusing if you didn't see the talk, but basically, it consisted of
+        me going through the slides for each of the sections and then showing the running
+        demo and walking through the code.
     `
 }, {
     name: "Dayton Data Visualization",
     date: "2016 - 2018",
+    preview: 'preview-ddv.png',
+    links: [],
     description: `
     `
 }, {
     name: "Americons",
     date: "2017",
-    url: "https://americons.us",
+    preview: 'preview-americons.png',
+    links: [{
+        url: 'https://americons.us',
+        title: 'Americons Website'
+    }],
     description: `
         description for americons goes here.
     `
--- src/pages/projects/project.js
+++ src/pages/projects/project.js
@@ -3,9 +3,29 @@ import React from 'react';
 export default (props) => {
     const { project } = props;
 
+    const bgStyle = {
+        'background-image': `url(/images/projects/${project.preview})`
+    };
+
     return (
         <div className="project">
-            { project.name }
+
+            <div className="project__preview" style={bgStyle}></div>
+
+            <div className="project__info">
+                <h2>{project.name}</h2>
+
+                <p>{project.description}</p>
+
+                <ul className="project__links">
+                    {project.links.map(link => {
+                        return (
+                            <li><a href={link.url}>{link.title}</a></li>
+                        )
+                    })}
+                </ul>
+            </div>
+
         </div>
     );
 };
