b682e68e3f21892fd045819c1795ce738da227c7
Michael Bowman <bowmanmc@gmail.com>
2020-01-18 19:30:10 -0500

    better navbar

--- src/pages/projects/descriptions.js
+++ src/pages/projects/descriptions.js
@@ -1,15 +1,21 @@
 
 export default [{
-    name: "Ramones",
-    date: "Fall 2019",
-    url: "https://ramones.itschilitime.com",
-    description: `
-        description for the ramones project goes here.
-    `
-}, {
-    name: "Cryptid Supply Company",
-    date: "Summer 2019",
-    url: "https://cryptidsupply.com",
+    //     name: "Ramones",
+    //     date: "Fall 2019",
+    //     url: "https://ramones.itschilitime.com",
+    //     description: `
+    //         description for the ramones project goes here.
+    //     `
+    // }, {
+    //     name: "Cryptid Supply Company",
+    //     date: "Summer 2019",
+    //     url: "https://cryptidsupply.com",
+    //     description: `
+    //     `
+    // }, {
+    name: "Data Visualization Workflows",
+    date: "Winter 2019",
+    url: "https://github.com/bowmanmc/dvworkflows",
     description: `
     `
 }, {
