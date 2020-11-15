cdb1f6481a7d3cf963a5921ddedd89917611b22c
Michael Bowman <bowmanmc@gmail.com>
2020-11-15 08:33:46 -0500

    add trailingSlash deployment option for aws

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@ const withMDX = require("@next/mdx")({
     extension: /\.mdx?$/
 });
 
-module.exports = withMDX({
-    pageExtensions: ["js", "jsx", "md", "mdx"]
+// module.exports = withMDX({
+//     pageExtensions: ["js", "jsx", "md", "mdx"]
+// });
+
+const mdxOpts = withMDX({
+    pageExtensions: ['js', 'jsx', 'md', 'mdx']
 });
+
+module.exports = {
+    ...mdxOpts,
+    trailingSlash: true,
+};
