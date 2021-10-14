47d843da7184ec3d3cbf564d50cde6d3f0914831
Michael Bowman <bowmanmc@gmail.com>
2021-10-14 16:11:50 -0400

    resume updates

--- components/ResumeExperience.js
+++ components/ResumeExperience.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 
 const ResumeExperience = (props) => {
-    const {company, title, start, end, description} = props;
+    const {company, title, start, end, children} = props;
 
     return (
         <div className="ResumeExperience">
             <h3>{title}</h3>
             <h4>{company}</h4>
             <h5>{start} - {end}</h5>
-            <p>{description}</p>
+            {children}
         </div>
     );
 };
--- pages/resume.js
+++ pages/resume.js
@@ -17,31 +17,16 @@ const resume = () => {
                 <section className="Resume__summary">
                     <h2>Summary</h2>
                     <p>
-                        I am an experienced engineering manager, technical team lead,
-                        application architect, fullstack software engineer, and educator
-                        from Dayton, Ohio, with expertise in delivering web-based data
-                        visualization applications to a wide variety of commercial and
-                        government clients. I am passionate about growing high performing
-                        engineering teams and working with them to unlock their potential
-                        to build maintainable software applications that delight end-users.
+                        I am an experienced engineering manager, technical team lead, application architect, fullstack software engineer, and educator from Dayton, Ohio, with expertise in delivering web-based data visualization applications to a wide variety of commercial and government clients. I am passionate about growing high performing engineering teams and working with them to unlock their potential to build maintainable software applications that delight end-users.
                     </p>
                     <p>
-                        For the last four years, I’ve worked at Mile Two, a young custom software
-                        design and development company. Currently, I am responsible for recruiting,
-                        leading, and mentoring an engineering staff of over twenty five,
-                        and overseeing the execution of ten project teams totaling over $5m in revenue.
+                        For the past four years, I’ve been at Mile Two, a young custom software design and development company, working with the founders to build out and grow a world class engineering team. I’ve been responsible for recruiting, hiring, leading, managing, and coaching a group of nearly sixty software engineers with diverse backgrounds and experience levels to perform at their best and collaborate as a team to deliver exceptional value for Mile Two’s clients.
                     </p>
                     <p>
-                        To help facilitate Mile Two’s rapid growth, I created the Mile Two Internship Program,
-                        building it up over the last four years into one of the top internship programs
-                        in the Dayton area. To date, we’ve brought thirty two interns through the program
-                        and offered full time positions to nineteen of them.
+                        To help facilitate Mile Two’s rapid growth, I created the Mile Two Internship Program, building it up over the last four years into one of the top internship programs in the Dayton area. To date, we’ve brought thirty two interns through the program and offered full time positions to nineteen of them.
                     </p>
                     <p>
-                        Since 2014 I have taught evening Computer Science courses at Sinclair Community College.
-                        I love helping students take their first steps towards a career in computer programming
-                        and am excited to see them entering the workforce (and have even had the pleasure of
-                        hiring a few of them).
+                        Since 2014 I have taught evening Computer Science courses at Sinclair Community College. I love helping students take their first steps towards a career in computer programming and am excited to see them entering the workforce (and have even had the pleasure of hiring a few of them).
                     </p>
                 </section>
 
@@ -93,155 +78,109 @@ const resume = () => {
                         company="Mile Two"
                         title="Portfolio Director"
                         start="September 2017"
-                        end="Present"
-                        description={`
-                        Helped grow the company from a rag-tag team of eight
-                        into an organization of over one hundred and twenty people.
-                        Led a portfolio consisting of over ten project teams working on
-                        various commercial and government contracts totaling over $5m in revenue.
-                        Managed a portfolio team consisting of over twenty staff members of
-                        varying roles including Project Leads, Software Engineers, UX/UI Designers,
-                        and Human Factors Engineers. Set the tone and direction for the
-                        engineering staff and helped foster a culture of continuous learning
-                        and improvement. Created and managed the Mile Two Internship Program,
-                        providing thirty two interns with an opportunity to work on cross-functional
-                        teams to design and build internal R&D software applications alongside
-                        our experienced staff. Committed to improving diversity and inclusion
-                        efforts at the company, setting and exceeding a goal of hiring 50%
-                        of our interns from under-represented groups in technology.
-                        `}
-                    />
+                        end="Present">
+                        <p>
+                            Provided empathetic, enthusiastic leadership while helping grow the company from a small team of eight into a mature organization of over one hundred and twenty people.
+                        </p>
+                        <p>
+                            As Portfolio Director I managed ten project teams working on various commercial and government contracts totaling over $5m in revenue. I managed a portfolio team consisting of over twenty staff members of varying roles including Project Leads, Engineering Managers, Software Engineers, QA Engineers, UX/UI Designers, and Human Factors Engineers. I was also responsible for managing the portfolio financials, budgets and projections, roadmap and vision, and business development activities. I coordinated with the other portfolios to adapt to changing customer and business priorities, ensuring our project teams were positioned well to deliver high quality custom applications to our customers.
+                        </p>
+                        <p>
+                            As Director of Engineering I set the tone and direction for the engineering staff and helped foster a culture of continuous learning and improvement. I managed the training budget and programs for over forty people, aligning the career goals of our software engineers with the strategic growth goals of the company (encouraging folks to take an interest and train in areas such as Data Science, AI/ML, and DevOps). Ensured project teams were following industry best practices and encouraging standardization across the organization on tools, frameworks, and development platforms.
+                        </p>
+                        <p>
+                            I also created and managed the Mile Two Internship Program, providing thirty two interns with an opportunity to work on cross-functional teams to design and build internal R&D software applications alongside our experienced staff. I am committed to improving diversity and inclusion efforts at the company, setting and exceeding a goal of hiring 50% of our interns from under-represented groups in technology.
+                        </p>
+                    </ResumeExperience>
 
                     <ResumeExperience
                         company="Sinclair Community College"
                         title="Adjunct Faculty"
                         start="January 2014"
-                        end="Present"
-                        description={`
-    Adjunct professor for the Computer Information Systems
-    department, teaching an evening programming course one night a
-    week. Instructed students from a wide range of backgrounds,
-    ages, and skill levels. Courses taught include Introduction to
-    Problem Solving and Computer Programming (C++), Data Analytics with Python,
-    Web Development with HTML and CSS, C++ Software Development,
-    and Java Software Development.
-    Volunteered to assist extracurricular hackathon
-    teams and served on the Software Engineering Advisory Board,
-    helping set the direction for curriculum and program updates.
-                        `}
-                    />
+                        end="Present">
+                        <p>
+                            Adjunct professor for the Computer Information Systems
+                            department, teaching an evening programming course one night a
+                            week. Instructed students from a wide range of backgrounds,
+                            ages, and skill levels. Courses taught include Introduction to
+                            Problem Solving and Computer Programming (C++), Data Analytics with Python,
+                            Web Development with HTML and CSS, C++ Software Development,
+                            and Java Software Development.
+                            Volunteered to assist extracurricular hackathon
+                            teams and served on the Software Engineering Advisory Board,
+                            helping set the direction for curriculum and program updates.
+                        </p>
+                    </ResumeExperience>
 
                     <ResumeExperience
                         company="Applied Information Sciences"
                         title="Principal Software Engineer"
                         start="January 2014"
-                        end="September 2017"
-                        description={`
-    Technical Team Lead and
-    Principal Software Engineer for
-    several contracts for the National Geospatial-Intelligence
-    Agency (NGA) and the National Air and Space Intelligence Center
-    (NASIC) developing web-based mapping and data visualization
-    applications for intelligence analysts. Worked closely with end
-    users to understand their job functions and needs first, then
-    built tools to help distill a large amount of complex data into
-    a useful format and answer key intelligence questions.
-                        `}
-                    />
+                        end="September 2017">
+                        <p>
+                            Technical Team Lead and Principal Software Engineer for several contracts for the National Geospatial-Intelligence Agency (NGA) and the National Air and Space Intelligence Center (NASIC) developing web-based mapping and data visualization applications for intelligence analysts. Worked closely with end users to understand their job functions and needs first, then built tools to help distill a large amount of complex data into a useful format and answer key intelligence questions.
+                        </p>
+                    </ResumeExperience>
 
                     <ResumeExperience
                         company="Booz Allen Hamilton"
                         title="Principal Software Engineer"
                         start="February 2011"
-                        end="January 2014"
-                        description={`
-    Lead software developer for NGA
-    Innovision’s GIAT Research and Development lab at NASIC. Led an
-    agile, ten-person team responsible for the rapid prototyping of
-    software to provide new capabilities for use across the
-    intelligence community. Performed a wide variety of software
-    development tasks, from gathering, mining, and warehousing
-    extremely large datasets to designing and developing
-    forward-looking user interfaces using front-end web
-    technologies.
-                        `}
-                    />
+                        end="January 2014">
+                        <p>
+                            Lead software developer for NGA Innovision’s GIAT Research and Development lab at NASIC. Led an agile, ten-person team responsible for the rapid prototyping of software to provide new capabilities for use across the intelligence community. Performed a wide variety of software development tasks, from gathering, mining, and warehousing extremely large datasets to designing and developing forward-looking user interfaces using front-end web technologies.
+                        </p>
+                    </ResumeExperience>
 
                     <ResumeExperience
                         company="Moreover Technologies"
                         title="Senior Software Engineer"
                         start="September 2010"
-                        end="February 2011"
-                        description={`
-    Developed software in Java for a news and social media startup.
-    Wrote specialized “Harvesters” for scraping specific types of
-    content off of websites (blogs, news articles, and used car
-    listings). Used cloud computing technologies (Amazon EC2) to
-    scale out content harvesting and data mining tasks. Worked as a
-    part of a distributed development team with members in Dayton,
-    Washington D.C., and London.
-                        `}
-                    />
+                        end="February 2011">
+                        <p>
+                        Developed software for a news and social media startup. Wrote specialized “Harvesters” for scraping specific types of content off of websites (news articles and used car listings). Utilized cloud computing technologies (Amazon EC2) to scale out content harvesting and mining tasks.
+                        </p>
+                    </ResumeExperience>
 
                     <ResumeExperience
                         company="General Dynamics"
                         title="Principal Software Engineer"
                         start="March 2007"
-                        end="September 2010"
-                        description={`
-    Worked as a technical lead for the
-    user interface team on a number of projects supporting NASIC. In
-    addition to my programming duties, I mentored junior members of
-    the team on software engineering best practices and drove the
-    adoption of agile software development techniques such as
-    Test-Driven Development, Continuous Integration, and Scrum,
-    leading to higher quality software developed faster.
-                        `}
-                    />
+                        end="September 2010">
+                        <p>
+                            Worked as a technical team lead on a number of projects supporting the National Air and Space Intelligence Center (NASIC) at Wright Patterson Air Force Base. In addition to my programming duties, I mentored junior members of the team on software engineering best practices and drove the adoption of agile software development techniques such as Test Driven Development, Continuous Integration, and Scrum, leading to higher quality software developed faster. Subject matter expert on developing web-based data visualization applications for intelligence analysts.
+                        </p>
+                    </ResumeExperience>
 
                     <ResumeExperience
                         company="Engyro"
                         title="Senior Software Engineer"
                         start="March 2006"
-                        end="March 2007"
-                        description={`
-    Developed several of the Engyro non-Windows monitoring agents
-    for Microsoft's System Center Operations Manager, allowing
-    customers to monitor their Linux/Unix servers and software
-    (such as Apache HTTPD and Oracle DBMS) alongside their Microsoft
-    assets. Worked in a very dynamic environment where schedules and
-    features were constantly changing based upon customer feedback
-    and needs. Engyro was acquired by Microsoft in March 2007.
-                        `}
-                    />
+                        end="March 2007">
+                        <p>
+                            Developed several of the Engyro non-Windows monitoring agents for Microsoft's System Center Operations Manager, allowing customers to monitor their Linux/Unix servers and software (such as Apache HTTPD and Oracle DBMS) alongside their Microsoft assets. Worked in a very dynamic environment where schedules and features were constantly changing based upon customer feedback and needs. Engyro was acquired by Microsoft in March 2007.
+                        </p>
+                    </ResumeExperience>
 
                     <ResumeExperience
                         company="Red Hat"
                         title="Software Engineer"
                         start="August 2004"
-                        end="March 2006"
-                        description={`
-    Worked on the Red Hat Network Satellite Server product helping to
-    deliver bug fixes and new features to customers on a tight release
-    schedule. I worked on the server-side team, responsible for developing
-    the web interface and APIs to allow system administrators to update
-    and manage a large network of Red Hat Linux machines easily. Day to
-    day activities consisted of writing code for the Java based web
-    application, documenting code and best practices, writing test plans,
-    preparing level of effort time estimates for project management, and
-    helping team members debug code.
-                        `}
-                    />
+                        end="March 2006">
+                        <p>
+                            Worked on the Red Hat Network Satellite Server product helping to deliver bug fixes and new features to customers on a tight release schedule. I worked on the server-side team, responsible for developing the web interface and APIs to allow system administrators to update and manage a large network of Red Hat Linux machines easily. Day to day activities consisted of writing code for the Java based web application, documenting code and best practices, writing test plans, preparing level of effort time estimates for project management, and helping team members debug code.
+                        </p>
+                    </ResumeExperience>
 
                     <ResumeExperience
                         company="Belcan"
                         title="Web Developer"
                         start="June 2001"
-                        end="August 2004"
-                        description={`
-    Developed and maintained corporate intranet site.
-                        `}
-                    />
+                        end="August 2004">
+                        <p>
+                            Responsible for intranet design, development, and maintenance. This involved prioritizing project requests from multiple customers, developing prototypes and collecting feedback, determining time and resource estimations, implementing the project, and maintaining the site after completion. Led a team of three web developers and was responsible for delegating tasks and responsibilities to members as projects were received from customers. Among sites developed were a document management system for ISO documentation, a portal providing news and company information, and a group management site providing dynamic workstation assignments, news and announcements, tips and tricks, and up- to-date software reference information.
+                        </p>
+                    </ResumeExperience>
                 </section>
 
                 <section className="Resume__community">
